f2d0ea7c79fb316a82caf5a85b6b737d208d707c
Madeuss <mateus1two3@gmail.com>
2026-02-20 01:03:45 -0300

    chore: delete that shit

--- types/env.d.ts
+++
@@ -1,3 +0,0 @@
-declare module '@env' {
-  export const SEVERUS_SERVER_API_ADDRESS: string
-}
--- utils/locales/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as translationEN } from './en/translation.json'
-export { default as translationES } from './es/translation.json'
-export { default as translationPT } from './pt/translation.json'
